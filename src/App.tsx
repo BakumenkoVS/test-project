c5033d00aa1ea38014867b5c6312e62f030d6825
vlad bakumenko <tinyyy11@yandex.ru>
2022-12-12 20:36:37 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import AuthPage from "./pages/AuthPage";
+import AirportDetailPage from "./pages/AirportDetailPage";
 
 function App() {
-   return <div>hello</div>;
+   return (
+      <div>
+         <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/airport/:id" element={<AirportDetailPage />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
